01f4132341d2f5ac7a381e201e19d515b243fdc4
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-10-14 18:34:35 -0300

    initial commit

---
+++ src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,30 @@
+package com.example.login_auth_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
