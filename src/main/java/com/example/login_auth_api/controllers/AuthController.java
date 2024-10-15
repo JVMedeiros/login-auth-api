8fdcee636c41a833d83c39a083c9b6df878cceea
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-10-15 00:01:58 -0300

    refactor: implementing lombok

--- src/main/java/com/example/login_auth_api/controllers/AuthController.java
+++ src/main/java/com/example/login_auth_api/controllers/AuthController.java
@@ -4,10 +4,10 @@ import com.example.login_auth_api.domain.user.User;
 import com.example.login_auth_api.dto.LoginRequestDTO;
 import com.example.login_auth_api.dto.RegisterRequestDTO;
 import com.example.login_auth_api.dto.ResponseDTO;
+import com.example.login_auth_api.exception.UserNotFoundException;
 import com.example.login_auth_api.infra.security.TokenService;
 import com.example.login_auth_api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,34 +21,35 @@ import java.util.Optional;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private UserRepository repository;
-    private PasswordEncoder passwordEncoder;
-    private TokenService tokenService;
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginRequestDTO body) {
-        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
-        if (passwordEncoder.matches(user.getPassword(), body.password())) {
+    public ResponseEntity<ResponseDTO> login(@RequestBody LoginRequestDTO body) {
+        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new UserNotFoundException("Usuário não encontrado!"));
+        if(passwordEncoder.matches(body.password(), user.getPassword())) {
             String token = this.tokenService.generateToken(user);
-            return  ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+            return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
         }
-        return  ResponseEntity.badRequest().build();
+        return ResponseEntity.badRequest().build();
     }
 
+
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody RegisterRequestDTO body) {
+    public ResponseEntity<ResponseDTO> register(@RequestBody RegisterRequestDTO body){
         Optional<User> user = this.repository.findByEmail(body.email());
 
         if(user.isEmpty()) {
             User newUser = new User();
-            newUser.setEmail(body.email());
             newUser.setPassword(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
             newUser.setName(body.name());
             this.repository.save(newUser);
 
             String token = this.tokenService.generateToken(newUser);
-            return  ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
         }
-        return  ResponseEntity.badRequest().build();
+        return ResponseEntity.badRequest().build();
     }
 }
